de87fe65f66c18aa2954d22473b9f136ba2f3c50
BleKuntay <riki.mcdougall@hotmail.com>
2026-03-16 05:23:11 +0700

    test: configure dynamic properties for Testcontainers PostgreSQL
    - Added `@DynamicPropertySource` to set JDBC URL, username, and password dynamically in tests.

--- src/test/java/cloud/chlora/management/ManagementServiceApplicationTests.java
+++ src/test/java/cloud/chlora/management/ManagementServiceApplicationTests.java
@@ -2,6 +2,8 @@ package cloud.chlora.management;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.postgresql.PostgreSQLContainer;
@@ -12,6 +14,13 @@ class ManagementServiceApplicationTests {
 
     @Container
     static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer("postgres:16");
+
+    @DynamicPropertySource
+    static void configure(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+    }
 
     @Test
     void contextLoads() {
