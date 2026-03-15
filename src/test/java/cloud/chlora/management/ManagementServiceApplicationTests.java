43831a2c4b9d8ef7a6bf3616d81e2a3c223fbd23
BleKuntay <riki.mcdougall@hotmail.com>
2026-03-16 05:18:23 +0700

    test: integrate Testcontainers for PostgreSQL in tests
    - Added Testcontainers dependency to `pom.xml`.
    - Configured static PostgreSQLContainer for test contexts in `ManagementServiceApplicationTests`.

--- src/test/java/cloud/chlora/management/ManagementServiceApplicationTests.java
+++ src/test/java/cloud/chlora/management/ManagementServiceApplicationTests.java
@@ -2,9 +2,16 @@ package cloud.chlora.management;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.postgresql.PostgreSQLContainer;
 
+@Testcontainers
 @SpringBootTest
 class ManagementServiceApplicationTests {
+
+    @Container
+    static PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer("postgres:16");
 
     @Test
     void contextLoads() {
